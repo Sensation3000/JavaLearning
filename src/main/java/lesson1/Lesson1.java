747ec3b34e2c9552441208c38d7cd65f4a92f0db
Sensation3000 <omsksmp@gmail.com>
2025-01-30 00:57:22 +0300

    hw1

--- src/main/java/lesson1/Lesson1.java
+++ src/main/java/lesson1/Lesson1.java
@@ -1,7 +1,10 @@
-public class Main {
+package lesson1;
+
+public class Lesson1 {
     public static void main(String[] args) {
         System.out.println("hello\n" + "+ 1");
         System.out.println("Hello" + "world");
         System.out.println("hello\" world");
+        System.out.printf("Процент жирности молока : %d%%", 10);
         }
     }
