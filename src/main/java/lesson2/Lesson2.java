7e0dd85354099dec0db20dafe776402077e90ef6
Sensation3000 <omsksmp@gmail.com>
2025-02-04 01:17:18 +0300

    lesson2 second part

--- src/main/java/lesson2/Lesson2.java
+++ src/main/java/lesson2/Lesson2.java
@@ -6,6 +6,7 @@ public class Lesson2 {
 
         int x; //тип переменной int и название переменной
         x = 1234;   //в эту переменную кладем значение. Знак равно это оператор присваивания
+        x = x + 1;  //можно переприсваивать данные в переменную
         int y = 4467;
         System.out.println(x + y); //достали значения из переменных, сложили и только полученный результат распечатали
 
@@ -51,7 +52,21 @@ public class Lesson2 {
         int remainder = oranges2 % 2; //остаток от целочисленного деления
         System.out.println(remainder);
 
+        //CHAR
+        char f = 80;
+        System.out.println(f); //напечатает символ P т.к код 80 соответствует этой латинской букве
 
+        char r = 100;
+        char d = 'd';   //пишем в одинарных кавычках
+        System.out.println(r == d); // true т.к они идентичны
+        System.out.println(r);  //напечатает символ d
+        System.out.println(d);  //напечатает символ d
 
+        char letterD = 'd';
+        char result = (char) (letterD +1);  // 100+1 перевод в char
+        System.out.println(result);   //напечатает символ e
+
+        int a1 = 100;
+        System.out.println((char) a1);  //напечатает символ d  т.к мы перевели значение переменной типа int в char
     }
 }
