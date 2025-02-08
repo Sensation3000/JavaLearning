ed4bab940b6876052c5986ddaa71d58fd70fcc6c
Sensation3000 <116028836+Sensation3000@users.noreply.github.com>
2025-02-09 00:55:43 +0300

    Lesson3 (#11)

--- src/main/java/homework4/homework4_2.java
+++ src/main/java/homework4/homework4_2.java
@@ -3,15 +3,33 @@ package homework4;
 public class homework4_2 {
     public static void main(String[] args) {
         //Задача №2
-        //
         //Необходимо вывести все положительные степени числа 5 до тех пор,
         // пока результат возведения в степень меньше 100000,
         // вывести результат возведения в степень.
 
+        //1 вариант
         int result = 1;
         while (result < 100000){
             result = result * 5;
+            if (result > 100000){
+                break;
+            }
             System.out.println(result);
+        }
+
+//        2 вариант
+        int number = 5;
+        int res = 1;
+
+        for (int i = 1; res * number < 100000; i++) {
+            res *= number;
+            System.out.println(res);
+        }
+
+//        3 вариант
+        int num = 5;
+        for (int i = 1; i < 100000; i *= num) {
+            System.out.println(i);
         }
     }
 }
---
+++ src/main/java/lesson3/Lesson3_2.java
@@ -0,0 +1,45 @@
+package lesson3;
+
+public class Lesson3_2 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        boolean res = x == y; //строгое сравнение
+        //>= больше или равно
+        //<= меньше или равно
+
+        //приоритет операторов - умножение первое, сложение второе.
+        //у операторов сравнения наименьший приоритет
+        System.out.println(res);
+        System.out.println(!res);//не равно
+        System.out.println(!(x == y));// не равно применяем к выражению
+
+        if (!(x < y)){
+            System.out.println("true!");
+        } else {
+            System.out.println("false!");
+        }
+
+        //or
+        // true or true == true
+        // true or false == true
+        // false or true == true
+        // false or false == false
+
+        if (x == y || x + 2 > 3){
+            System.out.println("or!");
+        }
+
+        //and
+        // true and true == true
+        // true and false == false
+        // false and true == false
+        // false and false == false
+
+        if (x == y && x + 2 < 10){
+            System.out.println("and!");
+        } else {
+            System.out.println("false!");
+        }
+    }
+}
