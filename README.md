10d183d684636c60a60ae8b026a509b128ede502
Sensation3000 <omsksmp@gmail.com>
2025-02-11 20:08:40 +0300

    lesson4 first part

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 #### with RedRover.School https://t.me/RedRoverSchool
 
 * Lesson 1 - IDEA install and first project
-* Lesson 2 - Variable, numeric datatypes
+* Lesson 2 - Variables, numeric datatypes
 * Lesson 3 - Boolean and if
+* Lesson 3 - Loops
---
+++ src/main/java/lesson4/Lesson4_1.java
@@ -0,0 +1,50 @@
+package lesson4;
+
+public class Lesson4_1 {
+    public static void main(String[] args) {
+        boolean itMoves = true;
+        boolean itShouldMove = false;
+
+        //1 вариант
+        //все понятно,но есть повторяющийся код
+        if (itMoves){
+            if(itShouldMove){
+                System.out.println("No Problem");
+            } else {
+                System.out.println("Use duct tape!");
+            }
+        } else {
+            if (itShouldMove){
+                System.out.println("Use WD-40!");
+            } else {
+                System.out.println("No problem!");
+            }
+        }
+
+        //2 вариант
+        // c использованием операторов or и and
+        if ((itMoves && itShouldMove) || (!itMoves && !itShouldMove)){
+            System.out.println("No problem!");
+        } else {
+            if (!itMoves) {
+                System.out.println("Use WD-40!");
+            } else {
+                System.out.println("Use duct tape!");
+            }
+        }
+
+        //3 вариант
+        // еще больше упрощаем
+        if ((itMoves == itShouldMove)){
+            System.out.println("No problem!");
+        } else {
+            if (!itMoves) {
+                System.out.println("Use WD-40!");
+            } else {
+                System.out.println("Use duct tape!");
+            }
+        }
+
+
+    }
+}
