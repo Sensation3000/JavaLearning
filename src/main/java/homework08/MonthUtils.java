100de56a5ea5f70eb2b2a747d87fffbcc3946b67
Sensation3000 <116028836+Sensation3000@users.noreply.github.com>
2025-02-28 12:11:19 +0300

    Hw8 refactor2 (#24)

--- src/main/java/homework08/MonthUtils.java
+++ src/main/java/homework08/MonthUtils.java
@@ -1,19 +1,21 @@
 package homework08;
 
-
 public class MonthUtils {
-    static Month january = makeMonth("January", 31, 23);
-    static Month february = makeMonth("February", 28, 20);
-    static Month march = makeMonth("March", 31, 22);
-    static Month april = makeMonth("April", 30, 22);
-    static Month may = makeMonth("May", 31, 22);
-    static Month june = makeMonth("June", 30, 21);
-    static Month july = makeMonth("July", 31, 22);
-    static Month august = makeMonth("August", 31, 23);
-    static Month september = makeMonth("September", 30, 22);
-    static Month october = makeMonth("October", 31, 23);
-    static Month november = makeMonth("November", 30, 20);
-    static Month december = makeMonth("December", 31, 23);
+
+    public static Month[] monthArray = {
+            makeMonth("January", 31, 23),
+            makeMonth("February", 28, 20),
+            makeMonth("March", 31, 22),
+            makeMonth("April", 30, 22),
+            makeMonth("May", 31, 22),
+            makeMonth("June", 30, 21),
+            makeMonth("July", 31, 22),
+            makeMonth("August", 31, 23),
+            makeMonth("September", 30, 22),
+            makeMonth("October", 31, 23),
+            makeMonth("November", 30, 20),
+            makeMonth("December", 31, 23),
+    };
 
     //factory method
     static Month makeMonth(String monthName, int numberOfDays, int numberOfWorkDays) {
@@ -23,4 +25,16 @@ public class MonthUtils {
         month.numberOfWorkDays = numberOfWorkDays;
         return month;
     }
+
+/*
+    other way (when there is yet no knowledge about constructors or factory method
+    public static Month getJanuary () {
+        Month january = new Month();
+        january.monthName = "January";
+        january.numberOfWorkDays = 31;
+        january.numberOfDays = 23;
+        return january;
+    }
+*/
+
 }
