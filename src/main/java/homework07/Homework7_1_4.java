3704a51a09a630435783129fa7034e06b922ada3
Sensation3000 <116028836+Sensation3000@users.noreply.github.com>
2025-08-24 13:16:03 +0300

    HW7 refactor (#30)

--- src/main/java/homework07/Homework7_1_4.java
+++ src/main/java/homework07/Homework7_1_4.java
@@ -6,14 +6,13 @@ public class Homework7_1_4 {
 //        Дан массив:
 //        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 //        необходимо вывести среднее арифметическое всех значений массива.
-        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         double average = sum * 1.0 / array.length;
-        System.out.println(sum);
         System.out.println(average);
     }
 }
